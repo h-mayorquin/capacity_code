f3b37ab062b112793c0c6d0519ed0951172e5e9d
h-mayorquin <h.mayorquin@gmail.com>
2019-10-25 18:17:05 +0200

    This

---
+++ job_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+
+#SBATCH -A 2018-3-582
+#SBATCH -t 02:00:00
+
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=32
+
+module load mpi4py/3.0.2/py37
+aprun -n 128 -N 32 python mpi_test.py
