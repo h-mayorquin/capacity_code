a28fa230cc40ea9e55373af9f38a2a8faeeb83db
h-mayorquin <h.mayorquin@gmail.com>
2019-11-25 15:27:05 +0100

    Sequence curve

--- sequence_curve.py
+++ sequence_curve.py
@@ -24,7 +24,7 @@ hypercolumns = int(sys.argv[2])
 minicolumns = int(sys.argv[3])
 sequence_length = int(sys.argv[1])
 recall_dynamics = sys.argv[6]
-print('recall_dynamics')
+tau_z_slow = 0.005
 memory=True
 number_of_sequences = 3
 total_trials = 100
@@ -43,7 +43,7 @@ if rank == 0:
 pattern_seed = rank
 for ns in number_of_sequences_vector:
     
-    aux = serial_wrapper(trials_per_rank, hypercolumns, minicolumns, ns, sequence_length, pattern_seed, tau_z_pre, tau_a, memory, recall_dynamics)
+    aux = serial_wrapper(trials_per_rank, hypercolumns, minicolumns, ns, sequence_length, pattern_seed, tau_z_pre, tau_z_slow, tau_a, memory, recall_dynamics)
     successes, points_of_failure, persistence_times, seq_recalled_pairs = aux
     
     aux_success = comm.gather(successes, root=0)
