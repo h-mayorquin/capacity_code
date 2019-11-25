12157bae8b6a154a315f259db6705a58134a7f1c
h-mayorquin <h.mayorquin@gmail.com>
2019-11-25 10:26:04 +0100

    Sequence caller

--- sequence_caller.sh
+++ sequence_caller.sh
@@ -16,6 +16,8 @@ minicolumns=10
 tau_z_pre=0.050
 tau_a=0.150
 
+output="./data_h$hypercolumns_m$minicolumns$_sl$sequence_length_z$tau_z_pre.pickle"
+
 module load mpi4py/3.0.2/py37
 #aprun -n 128 -N 32 python ./mpi_test.py > my_output_file.txt
 srun -n 128 python ./sequence_curve.py $sequence_length $hypercolumns $minicolumns $tau_z_pre $tau_a $output
