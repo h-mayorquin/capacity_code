77cceb23e5a6e84821fc5dd77af3b5dff9ca14d8
heberto <h.mayorquin@gmail.com>
2020-01-03 13:04:41 +0100

    Corrected the output file

--- sequence_caller.sh
+++ sequence_caller.sh
@@ -13,7 +13,7 @@
 hypercolumns=10
 minicolumns=10
 sequence_length=4
-tau_z_pre=0.050
+tau_z_pre=0.025
 sigma=0
 tau_a=0.150
 g_a=2.0
@@ -21,7 +21,7 @@ T_start=0.075
 T_per_sequence=0.050
 recall_dynamics="normala"
 
-output="./data_h${hypercolumns}_m${minicolumns}_sl${sequence_length}_z${tau_z_pre}_sigma${sigma}a${tau_a}_${recall_dynamics}.pickle"
+output="./data_h${hypercolumns}_m${minicolumns}_sl${sequence_length}_z${tau_z_pre}_sigma${sigma}_a${tau_a}_${recall_dynamics}.pickle"
 
 module load mpi4py/3.0.2/py37
 #aprun -n 128 -N 32 python ./mpi_test.py > my_output_file.txt
